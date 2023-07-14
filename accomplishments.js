8833d20e0568af89ebbfcc160bc528203aa6d7ff
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-07-14 12:42:40 +0530

    stylesheed and js file for accomplishment section updated

--- accomplishments.js
+++ accomplishments.js
@@ -3,10 +3,7 @@ function timelineAnimate(){
     function isElementInViewport(el) {
         let rect = el.getBoundingClientRect();
         return (
-            rect.top >= document.getElementById('TopBar').clientHeight &&
-            rect.left >= 0 &&
-            rect.bottom <=
-            (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
             rect.right <= (window.innerWidth || document.documentElement.clientWidth)
         );
     }
@@ -23,3 +20,50 @@ function timelineAnimate(){
     window.addEventListener("scroll", slideIn);
     window.addEventListener("resize", slideIn);
 };
+const accomplisments=[
+  {
+      time:2001,
+      content:"Lorem ipsum dolor sit amet consectetur adipisicing                     elit. Incidunt nostrum quidem eius repudiandae nam, nemo fuga                   architecto possimus id at rem beatae consequuntur dolor commodi."
+  },
+  {
+      time:2001,
+      content:"Lorem ipsum dolor sit amet consectetur adipisicing                     elit. Incidunt nostrum quidem eius repudiandae nam, nemo fuga                   architecto possimus id at rem beatae consequuntur dolor commodi."
+  },
+  {
+      time:2001,
+      content:"Lorem ipsum dolor sit amet consectetur adipisicing                     elit. Incidunt nostrum quidem eius repudiandae nam, nemo fuga                   architecto possimus id at rem beatae consequuntur dolor commodi."
+  },
+  {
+      time:2001,
+      content:"Lorem ipsum dolor sit amet consectetur adipisicing                     elit. Incidunt nostrum quidem eius repudiandae nam, nemo fuga                   architecto possimus id at rem beatae consequuntur dolor commodi."
+  },
+  {
+      time:2001,
+      content:"Lorem ipsum dolor sit amet consectetur adipisicing                     elit. Incidunt nostrum quidem eius repudiandae nam, nemo fuga                   architecto possimus id at rem beatae consequuntur dolor commodi."
+  },
+]
+
+function timeline()
+{
+    const container=document.getElementById('timeline');
+    const innerContainer=document.createElement('ul');
+    container.appendChild(innerContainer);
+    accomplisments.forEach(addAccomplishment);
+    function addAccomplishment(element)
+    {
+        const listitem=document.createElement('li');
+        const divitem=document.createElement('div');
+        const timeitem=document.createElement('time');
+        innerContainer.appendChild(listitem);
+        listitem.appendChild(divitem);
+        divitem.appendChild(timeitem);
+        timeitem.innerText=element.time;
+        divitem.innerHTML+=element.content;
+    } 
+    (function(){
+        var x =document.getElementById('accomplishments');
+        // console.log(x.offsetHeight/window.innerHeight*100);
+        x.style.setProperty('--before-height',x.offsetHeight+"px");
+        timelineAnimate();
+    })();
+}
