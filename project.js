ddd9e8998b66cb67282d31e28568b41bee07e600
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-07-17 23:06:34 +0530

    scroll is disabled when project placard is expanded

--- project.js
+++ project.js
@@ -2,6 +2,7 @@ function fun1()
 {
     if(width>1000)
     {
+        disable();
         document.getElementsByClassName('placard')[0].classList.add('placardExpanded');
         var x=document.getElementById('overlay2');
         x.classList.add("overlay2");
@@ -14,5 +15,20 @@ function fun2()
         document.getElementsByClassName('placard')[0].classList.remove('placardExpanded');
         var x=document.getElementById('overlay2');
         x.classList.remove("overlay2");
+        enable();
     }
+}
+function disable() {
+    // To get the scroll position of current webpage
+    TopScroll = window.pageYOffset || document.documentElement.scrollTop;
+    LeftScroll = window.pageXOffset || document.documentElement.scrollLeft,
+    
+    // if scroll happens, set it to the previous value
+    window.onscroll = function() {
+    window.scrollTo(LeftScroll, TopScroll);
+            };
+}
+    
+function enable() {
+    window.onscroll = function() {};
 }
