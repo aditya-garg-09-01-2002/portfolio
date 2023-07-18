a6b20de377ded7c858388370e44ea7eb5adacc6a
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-07-19 02:38:30 +0530

    changed js file for contactForm(some issues earlier)

--- aboutMe.js
+++ aboutMe.js
@@ -99,28 +99,3 @@ function addSocials()
     }
 }
 
-function overrideSubmit(event){
-    event.preventDefault();
-    alert("roboya says, this form is not working due to backend issues");
-}
-function validateMail(event)
-{
-    var x=document.getElementById("mail");
-    if(event.key==" "||event.code==="Space"||event.keyCode==32)event.preventDefault();
-    else{
-        const emailRegex=/^[a-z0-9.]+@+[a-z]+(\.[a-z]+)+$/;
-        if(emailRegex.test(x.value+event.key))document.getElementById("mail").style.backgroundColor="rgba(0,255,0,0.3)";
-        else document.getElementById("mail").style.backgroundColor="rgba(255,0,0,0.3)";
-    }
-}
-function validateName(event,x)
-{
-    var y;
-    if(x==='f')y=document.getElementById("fname");
-    else if(x==='l')y=document.getElementById("lname");
-    if(event.key==" "||event.code=="Space"||event.keyCode==32)event.preventDefault();
-    else{
-        const nameRegex=/^[a-z0-9A-Z]+$/;
-        if(!nameRegex.test(y.value+event.key))event.preventDefault();
-    }
-}
