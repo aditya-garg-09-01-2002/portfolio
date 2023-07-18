56336c924a536cca2906ae8d626ffc6be42bf7a3
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-07-19 02:37:02 +0530

    changed js file for contactForm

--- contactForm.js
+++ contactForm.js
@@ -23,3 +23,28 @@ function projectNo(){
     document.getElementById('projectDomain').style.animationName="bringOutProjectDomain";
     document.getElementById('submitButton').style.animationName="shiftMessageUp";
 }
+function overrideSubmit(event){
+    event.preventDefault();
+    alert("roboya says, this form is not working due to backend issues");
+}
+function validateMail(event)
+{
+    var x=document.getElementById("mail");
+    if(event.key==" "||event.code==="Space"||event.keyCode==32)event.preventDefault();
+    else{
+        const emailRegex=/^[a-z0-9.]+@+[a-z]+(\.[a-z]+)+$/;
+        if(emailRegex.test(x.value+event.key))document.getElementById("mail").style.backgroundColor="rgba(0,255,0,0.3)";
+        else document.getElementById("mail").style.backgroundColor="rgba(255,0,0,0.3)";
+    }
+}
+function validateName(event,x)
+{
+    var y;
+    if(x==='f')y=document.getElementById("fname");
+    else if(x==='l')y=document.getElementById("lname");
+    if(event.key==" "||event.code=="Space"||event.keyCode==32)event.preventDefault();
+    else{
+        const nameRegex=/^[a-z0-9A-Z]+$/;
+        if(!nameRegex.test(y.value+event.key))event.preventDefault();
+    }
+}
