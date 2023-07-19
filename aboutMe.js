0706050609223fcc3c3ecc5e64b35762a899c2c8
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-07-19 21:30:26 +0530

    updated aboutMe js file to module and created static backend

--- aboutMe.js
+++ aboutMe.js
@@ -1,74 +1,10 @@
-var z=document.getElementsByClassName('cards');
-z=z[1];
-const socials=[
-    {
-        target:"https://www.linkedin.com/in/aditya-garg-3175a4236/",
-        logoname:"fa-linkedin",
-        bgcolor:"#0077B5",
-        tcolor:"white",
-    },
-    {
-        target:"https://github.com/aditya-garg-09-01-2002",
-        logoname:"fa-github-square",
-        bgcolor:"#171515",
-        tcolor:"white",
-    },
-    {
-        target:"https://www.instagram.com/adityagarg_9102/",
-        logoname:"fa-instagram",
-        tcolor:"white",
-        bgcolor:"radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%)"
-    },
-    {
-        target:"https://www.facebook.com/adit.garg.921/",
-        logoname:"fa-facebook",
-        tcolor:"white",
-        bgcolor:"#3b5998"
-    },
-    {
-        target:"https://twitter.com/AdityaGarg9102",
-        logoname:"fa-twitter",
-        tcolor:"white",
-        bgcolor:"#00acee"
-    },
-    {
-        target:"https://www.youtube.com/channel/UCtGIZ-fASH4_7XWQ9mhgEwA",
-        logoname:"fa-youtube",
-        tcolor:"white",
-        bgcolor:" #c4302b"
-    },
-    {
-        target:"https://leetcode.com/gargaditya914/",
-        logoname:"cib-leetcode",
-        tcolor:"white",
-        bgcolor:"linear-gradient(to right, #000000 0%,#ffa116 90% , #b3b3b3 100%)"
-    },
-    {
-        target:"https://codeforces.com/profile/Exhaustic",
-        logoname:"fa-code",
-        tcolor:"white",
-        link:"code-forces.png",
-        bgcolor:"linear-gradient(to right, #fad261 23%,#188bcb 47%, #bc1d25 82%)"
-    },
-    {
-        target:"https://codeforces.com/profile/aditya_garg_09_01_2002",
-        logoname:"fa-code",
-        tcolor:"white",
-        link:"code-forces.png",
-        bgcolor:"linear-gradient(to right, #fad261 23%,#188bcb 47%, #bc1d25 82%)"
-    },
-    {
-        target:"https://www.codechef.com/users/aditya_garg_11",
-        link:"codechef.png",
-        logoname:"fa-code",
-        tcolor:"white",
-        bgcolor:"#453424"
-    }
-]
-function addSocials()
+import {socialList} from "./data/social.js";
+export function addSocials()
 {
+    var z=document.getElementsByClassName('cards');
+    z=z[1];
     const x =document.getElementById('smcontainer');
-    socials.forEach(addSocial);
+    socialList.forEach(addSocial);
     function addSocial(element)
     {
         var a=document.createElement('div');
