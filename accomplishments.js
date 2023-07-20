107fbb695230f9f9112da42aed387365e574b8a4
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-07-20 12:52:57 +0530

    removed scroll event handler from accomplishment js file

--- accomplishments.js
+++ accomplishments.js
@@ -1,28 +1,5 @@
 import {accomplishmentList} from "./data/accomplishment.js";
 
-function timelineAnimate(){
-    const items=document.querySelectorAll("#timeline ul li");
-    function isElementInViewport(el) {
-        let rect = el.getBoundingClientRect();
-        return (
-            rect.top >= 0 && rect.left >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-        );
-    }
-    function slideIn() {
-        for (let i = 0; i < items.length; i++) {
-          if (isElementInViewport(items[i])) {
-            items[i].classList.add("slide-in");
-          } else {
-            items[i].classList.remove("slide-in");
-          }
-        }
-    }
-    window.addEventListener("load", slideIn);
-    window.addEventListener("scroll", slideIn);
-    window.addEventListener("resize", slideIn);
-};
-
 export function timeline()
 {
     const container=document.getElementById('timeline');
@@ -44,6 +21,5 @@ export function timeline()
         var x =document.getElementById('accomplishments');
         // console.log(x.offsetHeight/window.innerHeight*100);
         x.style.setProperty('--before-height',x.offsetHeight+"px");
-        timelineAnimate();
     })();
 }
