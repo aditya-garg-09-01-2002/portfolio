4b616ebbed0045304583e45e4964ab07858ca1f3
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-07-21 00:10:50 +0530

    updated data for project section

--- data/project.js
+++ data/project.js
@@ -1,9 +1,40 @@
 export const projectList=[
     {
         title:"Game Automation using Python",
-        desc:"Give what project is",
+        placardExpand:1,
+        desc:"A Game Automation Script (or simply Game Bot) written entirely in Python which performs multiple tasks to be performed in a dynamic game enviornment, integrating OpenCV Image Detection to generate current state inputs and bring in suitable responses.<br>A 5-day haul where I grasped core concepts of I/O Event Handling and Image Detection from scratch and applied them to real-life use.<!--<br>Find out details on ...--!>",
         media:"<iframe src=\"https://www.youtube.com/embed/gQdrBmnemHY?rel=0\" allowfullscreen class=\"placard\"  onmouseleave=\"fun2()\"></iframe>",
-        links:["give link to github","embed linkedin"],
-        skills:"give what skills were incorporated",
+        links:
+        [
+            {
+                target:"linkedin",
+                url:"https://www.linkedin.com/posts/aditya-garg-3175a4236_github-aditya-garg-09-01-2002gameautomation-activity-7068908740049068032-6ArS?utm_source=share&utm_medium=member_desktop"
+            },
+            {
+                target:"github",
+                url:"https://github.com/aditya-garg-09-01-2002/game_automation"
+            }
+        ],
+        skills:["Python","Image Detection", "GitHub","Pyautogui","I/O Handling"],
+        projectCategory:["Game Automation","Software Development"],
+    },
+    {
+        placardExpand:0,
+        title:"Personal Portfolio Web Development Project",
+        desc:"A personal portfolio website wherein I have used HTML, CSS & JavaScript to create this website with mobile responsiveness also curated.<br>This website has also Fire Base as back-end to receive responses you can give me at the <a href=\"#contactForm\">form</a> below.<br>Be sure to drop a response so I can know how you experiences the website.",
+        media:"<img src=\"portfolioRepo.png\" class=\"placard\"></iframe>",
+        links:
+        [
+            {
+                target:"linkedin",
+                url:"#"
+            },
+            {
+                target:"github",
+                url:"https://github.com/aditya-garg-09-01-2002/portfolio"
+            }
+        ],
+        skills:["HTML","CSS", "JavaScript","Fire Base","GitHub"],
+        projectCategory:["Web Development"],
     }
 ];
