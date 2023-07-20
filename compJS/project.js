7964d972cf3818c8fd4db151c8f7276205838a6a
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-07-21 00:11:39 +0530

    updated js file to create and add tags with data in project section

--- compJS/project.js
+++ compJS/project.js
@@ -19,26 +19,119 @@ export function projectPopulate(){
             project.appendChild(projectRight);
             project.appendChild(projectBottom);
             projectTitle=document.createElement('div');
+            projectTitle.style.fontSize=24+"px";
             projectDesc=document.createElement('div');
             projectLeft.appendChild(projectTitle);
             projectLeft.appendChild(projectDesc);
             projectTitle.style.marginBottom=10+"px";
-            projectRight.setAttribute('onmouseenter',"fun1()");
+            if(element.placardExpand)projectRight.setAttribute('onmouseenter',"fun1()");
         })();
         //this function adds the data in the above created tags
         (function(){
             projectTitle.innerText=element.title;
-            projectDesc.innerText=element.desc;
+            projectDesc.innerHTML=element.desc;
             projectRight.innerHTML=element.media;
-            var temp=document.createElement('div');
-            temp.innerHTML=element.skills;
-            projectBottom.appendChild(temp);
-            element.links.forEach(addingLinks);
-            function addingLinks(link){
+            var temp,innerTemp;
+            //the function below is to add project domains
+            (function(){
                 temp=document.createElement('div');
-                temp.innerHTML=link;
+                temp.style.display="flex";
+                temp.style.flexWrap="Wrap";
+                temp.style.alignItems="Center";
+                temp.style.width="100%";
+                innerTemp=document.createElement('span');
+                innerTemp.innerText="Project Domain(s)";
+                innerTemp.style.marginBottom="10px";
+                innerTemp.style.marginLeft="10px";
+                innerTemp.style.marginRight="10px";
+                temp.appendChild(innerTemp);
+                element.projectCategory.forEach(function(e){
+                    innerTemp=document.createElement('span');
+                    innerTemp.style.width="auto";
+                    innerTemp.innerText=e;
+                    innerTemp.style.textShadow="none";
+                    innerTemp.style.marginBottom="10px";
+                    innerTemp.style.marginLeft="10px";
+                    innerTemp.style.marginRight="10px";
+                    innerTemp.style.padding="5px 5px";
+                    innerTemp.style.boxSizing="border-box";
+                    innerTemp.style.boxShadow="1px 1px 5px black,1px -1px 5px black,-1px 1px 5px black, -1px -1px 5px black "
+                    innerTemp.style.borderRadius="10%";
+                    innerTemp.style.backgroundColor="rgba(0,0,0,0.5)"
+                    temp.appendChild(innerTemp);
+                });
                 projectBottom.appendChild(temp);
-            }
+            })();
+            //the function below is to add skills involved in the project
+            (function(){
+                temp=document.createElement('div');
+                temp.style.display="flex";
+                temp.style.flexWrap="Wrap";
+                temp.style.alignItems="Center";
+                temp.style.width="100%";
+                innerTemp=document.createElement('span');
+                innerTemp.innerText="Skills";
+                innerTemp.style.marginBottom="10px";
+                innerTemp.style.marginLeft="10px";
+                innerTemp.style.marginRight="10px";
+                temp.appendChild(innerTemp);
+                element.skills.forEach(function(e){
+                    innerTemp=document.createElement('span');
+                    innerTemp.style.width="auto";
+                    innerTemp.style.textShadow="none";
+                    innerTemp.innerText=e;
+                    innerTemp.style.marginBottom="10px";
+                    innerTemp.style.marginLeft="10px";
+                    innerTemp.style.marginRight="10px";
+                    innerTemp.style.padding="5px 5px";
+                    innerTemp.style.boxSizing="border-box";
+                    innerTemp.style.boxShadow="1px 1px 5px black,1px -1px 5px black,-1px 1px 5px black, -1px -1px 5px black "
+                    innerTemp.style.borderRadius="10%";
+                    innerTemp.style.backgroundColor="rgba(0,0,0,0.5)"
+                    temp.appendChild(innerTemp);
+                });
+                projectBottom.appendChild(temp);
+            })();
+            //the function below is to add link embeds of the project
+            (function(){
+                
+                temp=document.createElement('div');
+                temp.style.display="flex";
+                temp.style.flexWrap="Wrap";
+                temp.style.alignItems="Center";
+                temp.style.width="100%";
+                innerTemp=document.createElement('span');
+                innerTemp.innerText="Links";
+                innerTemp.style.marginBottom="10px";
+                innerTemp.style.marginLeft="10px";
+                innerTemp.style.marginRight="10px";
+                temp.appendChild(innerTemp);
+                element.links.forEach(function(e){
+                    innerTemp=document.createElement('span');
+                    innerTemp.style.width="auto";
+                    innerTemp.style.marginBottom="10px";
+                    innerTemp.style.marginLeft="10px";
+                    innerTemp.style.textShadow="none";
+                    innerTemp.style.marginRight="10px";
+                    innerTemp.style.padding="5px 5px";
+                    innerTemp.style.boxSizing="border-box";
+                    innerTemp.style.boxShadow="1px 1px 5px black,1px -1px 5px black,-1px 1px 5px black, -1px -1px 5px black "
+                    innerTemp.style.borderRadius="10%";
+                    innerTemp.style.backgroundColor="rgba(0,0,0,0.5)"
+                    var anchor=document.createElement('a');
+                    anchor.href=e.url;
+                    var icon =document.createElement('i');
+                    icon.classList.add("fa");
+                    icon.classList.add("fa-"+e.target);
+                    innerTemp.appendChild(anchor);
+                    anchor.appendChild(icon);
+                    temp.appendChild(innerTemp);
+                    icon.style.setProperty("--t-color","white");
+                    anchor.style.textDecoration="none";
+                    icon.style.fontSize="30px";
+                });
+                projectBottom.appendChild(temp);
+            })();
         })();
     }
 }
