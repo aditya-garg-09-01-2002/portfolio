3d6cb6e65d286dd4f0c5982277d8b21617e3c60a
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-07-22 03:52:05 +0530

    updated disable and enable scroll logic

--- functions.js
+++ functions.js
@@ -3,7 +3,7 @@ function fun1()
     //this functions comes into play when project media placard is expaned i.e. hovered
     if(width>1000)
     {
-        disableScroll();
+        document.body.style.overflow="hidden";
         document.getElementsByClassName('placard')[0].classList.add('placardExpanded');
         var x=document.getElementById('overlay2');
         x.classList.add("overlay2");
@@ -17,21 +17,6 @@ function fun2()
         document.getElementsByClassName('placard')[0].classList.remove('placardExpanded');
         var x=document.getElementById('overlay2');
         x.classList.remove("overlay2");
-        enableScroll();
+        document.body.style.overflow="hidden auto";
     }
-}
-
-
-function disableScroll() {
-    // To get the scroll position of current webpage
-    TopScroll = window.pageYOffset || document.documentElement.scrollTop;
-    LeftScroll = window.pageXOffset || document.documentElement.scrollLeft,
-    
-    // if scroll happens, set it to the previous value
-    window.onscroll = function() {
-    window.scrollTo(LeftScroll, TopScroll);
-            };
-} 
-function enableScroll() {
-    window.onscroll = function() {};
 }
