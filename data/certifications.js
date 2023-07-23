fabbac0ecb7abae65047ddcf4a3c26faf7b383db
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-07-23 22:51:59 +0530

    added education data

--- data/certifications.js
+++ data/certifications.js
@@ -1,17 +1,16 @@
 export const certificateList=[
     {
+        data:"<pre style=\"white-space:pre-wrap\">Institue name : LNMIIT, Jaipur\n\nDegree : B.Tech in Computer Science and Engineering (CSE) (2025)\n\nCourses I excelled at:\n<ul><li>Computer Programming<li>Data Structures and Alogrithms<li>IDBMS<li>Computer Networks<li>Operating Systems<li>Optimisation Techniques and Algorithms<li>Advanced Programming<li>Computer Organisation and Architecture</ul>Skills I Acquired:<ul><li>Learnt Programming in C/C++, primarily used for Competitve Programming and Course related tasks.<li>Learnt Programming in Python, and developed my own Game Automation Project.<li>Learnt React, HTML, CSS, and JS and deployed my own portfolio website using GitHub as well.<li>Had a great experience in Omnetpp using in Computer Networks which made simulating seemless.<li>Practised Java a lot for OOPS and a lot of other programming.<li>Learnt MySQL and handling databases in depth<li>Practised on SimuLink and MATLAB for OTA and Architectures</ul>Club I Joined: GDSC LNMIIT as GDSC Competitive Programming Lead\n\nA lot has happened and a lot more is waiting.</pre>",
         name:"LNMIIT",
         course:"B.Tech in CSE",
         image:"./data/pictures/lnmiit.png"
     },
     {
-        name:"SPSEC",
-        course:"Class XII",
+        data:"<pre style=\"white-space:pre-wrap\">Institute name : Sir Padamapat Singhania Education Centre, Kanpur\nCompleted my Senior Secondary schooling i.e. Class XII with 95+ percentage.</pre>",
         image:"./data/pictures/spsec.png"
     },
     {
-        name:"SPSEC",
-        course:"Class X",
+        data:"<pre style=\"white-space:pre-wrap\">Institute name : Sir Padampat Singhania Education Centre, Kanpur\nCompleted my Secondary School i.e. class X with 97+ percentage.</pre>",
         image:"./data/pictures/spsec.png"
     }
 ]
