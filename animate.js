71aec08e91c480f4e267eb10982e1ab21d0a06fa
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-07-24 17:07:09 +0530

    rectified bug in accomplishment div animation(update not committed previously

--- animate.js
+++ animate.js
@@ -100,8 +100,8 @@ export function sectionCheck(){
                     {
                         var x=navbarHeight+30;
                         // if(window.innerHeight<1000)x=0;
-                        if(window.innerWidth<1000)x-=30;
-                        console.log(x);
+                        if(window.innerWidth<1000)x=0;
+                        // console.log(x);
                         if (isElementInViewport(accomplishmentDiv[i],x,-1,height,-1,false)) 
                             accomplishmentDiv[i].classList.add("slide-in");
                         else
