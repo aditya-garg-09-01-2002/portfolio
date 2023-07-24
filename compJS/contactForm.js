a4ab98302023c9407976ea9a4cd0040cbf5e7186
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-07-24 20:18:03 +0530

    removed contact form to backend connection bugs and inserted catch for errors

--- compJS/contactForm.js
+++ compJS/contactForm.js
@@ -43,23 +43,28 @@ var contactFormDB = firebase.database().ref("contactForm");
 const val = (id) => {
     return document.getElementById(id).value;
 };
-var mailcheck=0;
 function overrideSubmit(event){
     event.preventDefault();
     var flag=0;
-    if(val("name")==="")
+    var name=val("name");
+    var mail=val("mail");
+    if(name==="")
     {
         document.getElementById("name").style.backgroundColor="rgba(255,0,0,0.3)";
         flag=1;
     }
-    if(!mailcheck)
+    else if(!validateNameSubmit(name))
+    {
+        document.getElementById("name").style.backgroundColor="rgba(255,0,0,0.3)";
+        flag=1;
+    }
+    if(!validateMailSubmit(mail))
     {
         document.getElementById("mail").style.backgroundColor="rgba(255,0,0,0.3)";
         flag=1;
     }
+    // console.log(val("name")+" "+val("mail")+" "+flag);
     if(flag)return;
-    var name=val("fname");
-    var mail=val("mail");
     var projectstatus=document.querySelector("input[name='contactProject']:checked").value;
     var msg=val("desc");
     if(projectstatus==="yes")
@@ -68,12 +73,9 @@ function overrideSubmit(event){
         saveMessages(name,mail,projectstatus,Array.from(projectdomain, element => element.getAttribute('value')),JSON.stringify(msg));
     }
     else saveMessages(name,mail,projectstatus,"N/A",JSON.stringify(msg));
-    alert("Eager to read the message");
-    projectNo();
-    document.getElementById("contactForm").reset();
 }
 
-const saveMessages = (name,mail,projectstatus,projectdomain,msg) => {
+function saveMessages(name,mail,projectstatus,projectdomain,msg){
   var newContactForm = contactFormDB.push();
 
   newContactForm.set({
@@ -82,6 +84,15 @@ const saveMessages = (name,mail,projectstatus,projectdomain,msg) => {
     project: projectstatus,
     domains:projectdomain,
     message:msg
+  })
+  .then(()=>{
+    alert("Your message has been Well received.\nEager to read it!!!");
+    projectNo();
+    document.getElementById("contactForm").reset();
+    document.getElementById("mail").style.backgroundColor="transparent";
+  })
+  .catch(()=>{
+    alert("Currently experiencing some issues on back-end.\nPlease try again.");
   });
 };
 
@@ -91,8 +102,8 @@ function validateMail(event)
     if(event.key==" "||event.code==="Space"||event.keyCode==32)event.preventDefault();
     else{
         const emailRegex=/^[a-z0-9.]+@+[a-z]+(\.[a-z]+)+$/;
-        if(emailRegex.test(x.value+event.key)){document.getElementById("mail").style.backgroundColor="rgba(0,255,0,0.3)";mailcheck=1;}
-        else {document.getElementById("mail").style.backgroundColor="rgba(255,0,0,0.3)";mailcheck=0;};
+        if(emailRegex.test(x.value+event.key))document.getElementById("mail").style.backgroundColor="rgba(0,255,0,0.3)";
+        else document.getElementById("mail").style.backgroundColor="rgba(255,0,0,0.3)";
     }
 }
 function validateName(event)
@@ -101,4 +112,16 @@ function validateName(event)
     const nameRegex=/^[a-z0-9A-Z ]+$/;
     if(!nameRegex.test(y.value+event.key))event.preventDefault();
     else if(event.code!="Tab")y.style.backgroundColor="transparent";
+}
+function validateNameSubmit(e)
+{
+    const nameRegex=/^[a-z0-9A-Z ]+$/;
+    if(nameRegex.test(e))return 1;
+    return 0;
+}
+function validateMailSubmit(e)
+{
+    const emailRegex=/^[a-z0-9.]+@+[a-z]+(\.[a-z]+)+$/;
+    if(emailRegex.test(e))return 1;
+    return 0;
 }
